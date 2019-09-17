ef731f3669ac673ef335f00bb1490b8afe94ed42
Junzhuo Li <junzhuoli@JunzhuodeMacBook-Pro.local>
2019-09-16 21:44:30 -0600

    add node project

---
+++ node/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const art_express = require("express-art-template");
+
+var app = express();
+
+app.engine("art", art_express);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "art");
+
+app.get("/userlist", function(req, res) {
+    res.render("index.art", {
+        user: {
+            name: "aui",
+            tags: ["art", "template", "nodejs"]
+        }
+    });
+});
+
+app.listen(59999, () => {
+    console.log("192.168.1.66:59999/userlist")
+})
